3e17c2116edf58beeaf4406dd6aa20f5c38bc16b
a.zahedi <alireza.zahedikermani@gmail.com>
2025-11-09 13:29:18 +0330

    Fix mainActivity

--- app/src/main/java/com/example/incomingcallfcm/MainActivity.kt
+++ app/src/main/java/com/example/incomingcallfcm/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.incomingcallfcm
 
 import android.Manifest
 import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
